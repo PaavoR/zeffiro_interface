8c73b6c54210fa7f161041ecfaef647aa2d99e23
PaavoR <paavo.ronni@gmail.com>
2022-03-15 17:57:21 +0200

    Update createJob.m
    NumThreads added

--- cluster/example/createJob.m
+++ cluster/example/createJob.m
@@ -3,11 +3,15 @@
 c = parcluster;
 c.AdditionalProperties.AccountName = 'project_<number>';
 % MemUsage to memory per core (in MB)
-c.AdditionalProperties.MemUsage = '250000';
+c.AdditionalProperties.MemUsage = '12874';
 % 5 hour walltime
 c.AdditionalProperties.WallTime = '05:00:00';
 c.AdditionalProperties.EmailAddress = 'etu.suku@tuni.fi';
 c.AdditionalProperties.QueueName = 'small';
+% Number of computational threads for worker 
+c.NumThreads = 20;
+% if single worker
+% c.NumWorkers = 1;
 c.saveProfile
 
 %%
